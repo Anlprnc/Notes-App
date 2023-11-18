3e81430f810e8947efe99be7b20168961b6113dc
Anlprnc <anilpirincciii@gmail.com>
2023-11-18 18:49:47 +0300

    done

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const methodOverride = require('method-override');
 const connectDB = require('./server/config/db');
 const session = require('express-session');
 const passport = require('passport');
@@ -10,11 +11,24 @@ const MongoStore = require('connect-mongo');
 const app = express();
 const port = 5000 || process.env.PORT;
 
+app.use(
+  session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI,
+    }),
+    // cookie: { maxAge: new Date(Date.now() + 3600000) },
+  })
+);
+
 app.use(passport.initialize());
-// app.use(passport.session());
+app.use(passport.session());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride('_method'));
 
 connectDB();
 
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const User = require('../models/User');
 
 passport.use(
   new GoogleStrategy(
@@ -10,8 +11,27 @@ passport.use(
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: process.env.GOOGLE_CALLBACK_URL,
     },
-    function (accessToken, refreshToken, profile, cb) {
-      console.log(profile);
+    async function (accessToken, refreshToken, profile, done) {
+      const newUser = {
+        googleId: profile.id,
+        displayName: profile.displayName,
+        firstName: profile.name.givenName,
+        lastName: profile.name.familyName,
+        profileImage: profile.photos[0].value,
+      };
+
+      try {
+        let user = await User.findOne({ googleId: profile.id });
+
+        if (user) {
+          done(null, user);
+        } else {
+          user = await User.create(newUser);
+          done(null, user);
+        }
+      } catch (error) {
+        console.log(error);
+      }
     }
   )
 );
@@ -31,6 +51,30 @@ router.get(
 
 router.get('login-failure', (req, res) => {
   res.send('Something went wrong');
+});
+
+router.get('/logout', (req, res) => {
+  req.session.destroy((error) => {
+    if (error) {
+      console.log(error);
+      res.send('Error loggin out');
+    } else {
+      res.redirect('/');
+    }
+  });
+});
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async function (id, done) {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err, null);
+  }
 });
 
 module.exports = router;
